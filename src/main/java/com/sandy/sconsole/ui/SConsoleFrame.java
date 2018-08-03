f2861c1cf2f575a6818286d1eb5266c936406d0a
Sandeep Deb <deb.sandeep@gmail.com>
2018-08-03 22:10:24 +0530

    Hid the cursor

--- src/main/java/com/sandy/sconsole/ui/SConsoleFrame.java
+++ src/main/java/com/sandy/sconsole/ui/SConsoleFrame.java
@@ -3,6 +3,10 @@ package com.sandy.sconsole.ui;
 import java.awt.BorderLayout ;
 import java.awt.Color ;
 import java.awt.Container ;
+import java.awt.Cursor ;
+import java.awt.Point ;
+import java.awt.Toolkit ;
+import java.awt.image.BufferedImage ;
 
 import javax.swing.JFrame ;
 
@@ -30,6 +34,7 @@ public class SConsoleFrame extends JFrame {
         
         setUndecorated( true ) ;
         setResizable( false ) ;
+        hideCursor() ;
         
         contentPane.setBackground( Color.BLACK ) ;
         contentPane.setLayout( new BorderLayout() ) ;
@@ -37,5 +42,21 @@ public class SConsoleFrame extends JFrame {
         contentPane.add( dayTimePabel, BorderLayout.CENTER ) ;
         
         SwingUtils.setMaximized( this ) ;
+    }
+    
+    // The logic has been copied from a stack exchange post.
+    // https://stackoverflow.com/questions/1984071/how-to-hide-cursor-in-a-swing-application
+    private void hideCursor() {
+        // Transparent 16 x 16 pixel cursor image.
+        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+
+        // Create a new blank cursor.
+        Cursor blankCursor = Toolkit.getDefaultToolkit()
+                                    .createCustomCursor( cursorImg, 
+                                                         new Point(0, 0), 
+                                                         "blank cursor" ) ;
+
+        // Set the blank cursor to the JFrame.
+        contentPane.setCursor( blankCursor ) ;        
     }
 }
