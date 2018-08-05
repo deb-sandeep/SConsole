932399aac434128dd3defcaa5e1030edee5c6474
Sandeep Deb <deb.sandeep@gmail.com>
2018-08-05 20:51:38 +0530

    Got the skeleton layout of the remote working

---
+++ src/main/java/com/sandy/sconsole/api/HomeController.java
@@ -0,0 +1,13 @@
+package com.sandy.sconsole.api;
+
+import org.springframework.stereotype.Controller ;
+import org.springframework.web.bind.annotation.RequestMapping ;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping( "/" )
+    public String home() {
+        return "index" ;
+    }
+}
---
+++ src/main/resources/static/js/app/ng/app-ng.js
@@ -0,0 +1,7 @@
+var sConsoleApp = angular.module( 'sconsoleApp', 
+                                  [ 'ngRoute', 
+                                    'ui.bootstrap', 
+                                    'ngSanitize', 
+                                    'ui.indeterminate' 
+                                  ] ) ;
+
