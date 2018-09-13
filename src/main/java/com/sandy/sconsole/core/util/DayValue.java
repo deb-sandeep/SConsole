09cb741f485fd10323238e525efbe93fe84abe02
Sandeep Deb <deb.sandeep@gmail.com>
2018-09-13 14:50:53 +0530

    Queries for historic burns done

---
+++ src/main/java/com/sandy/sconsole/core/util/DayValue.java
@@ -0,0 +1,23 @@
+package com.sandy.sconsole.core.util;
+
+public class DayValue {
+
+    private String date = null ;
+    private float value = 0.0F ;
+    
+    public String getDate() {
+        return date ;
+    }
+    
+    public void setDate( String date ) {
+        this.date = date ;
+    }
+    
+    public float getValue() {
+        return value ;
+    }
+    
+    public void setValue( float value ) {
+        this.value = value ;
+    }
+}
--- src/main/java/com/sandy/sconsole/dao/repository/ProblemAttemptRepository.java
+++ src/main/java/com/sandy/sconsole/dao/repository/ProblemAttemptRepository.java
@@ -6,28 +6,29 @@ import org.springframework.data.jpa.repository.Query ;
 import org.springframework.data.repository.* ;
 
 import com.sandy.sconsole.api.burn.HistoricBurnStat ;
+import com.sandy.sconsole.core.util.DayValue ;
 import com.sandy.sconsole.dao.entity.* ;
 
 public interface ProblemAttemptRepository extends CrudRepository<ProblemAttempt, Integer> {
 
     @Query( nativeQuery=true,
-            value = "SELECT "
-                    + "    cast( pa.start_time as date ) as date, "
-                    + "    count( pa.problem_id ) as numQuestionsSolved, "
-                    + "    sum(duration) as duration "
-                    + "FROM "
-                    + "    problem_attempt pa, "
-                    + "    problem_master pm "
-                    + "WHERE "
-                    + "    pa.problem_id = pm.id and "
-                    + "    pm.topic_id = ?1 and "
-                    + "    ( pa.outcome = 'Solved' or pa.outcome = 'Ignore' ) "
-                    + "GROUP BY "
-                    + "    date" 
+            value =   
+              "SELECT "
+            + "    cast( pa.start_time as date ) as date, "
+            + "    count( pa.problem_id ) as numQuestionsSolved, "
+            + "    sum(duration) as duration "
+            + "FROM "
+            + "    problem_attempt pa, "
+            + "    problem_master pm "
+            + "WHERE "
+            + "    pa.problem_id = pm.id and "
+            + "    pm.topic_id = ?1 and "
+            + "    ( pa.outcome = 'Solved' or pa.outcome = 'Ignore' ) "
+            + "GROUP BY "
+            + "    date" 
     )
     public List<HistoricBurnStat> getHistoricBurnStats( Integer topicId ) ;
     
-   
     @Query( nativeQuery=true,
             value = 
               "SELECT "
@@ -42,4 +43,60 @@ public interface ProblemAttemptRepository extends CrudRepository<ProblemAttempt,
             + "    pa.start_time > current_date() "
     )
     public Integer getNumProblemsSolvedToday( Integer topicId ) ;
+    
+    @Query( nativeQuery=true, 
+            value = 
+              "SELECT "
+            + "    CAST( s.start_time as DATE ) as date, "
+            + "    sum( s.duration )/3600 as value "
+            + "FROM "
+            + "    session s, "
+            + "    topic_master tm "
+            + "WHERE "
+            + "    s.topic_id = tm.id AND "
+            + "    tm.subject_name = ?1 AND "
+            + "    s.start_time > DATE(DATE_SUB(NOW(), INTERVAL 30 DAY)) "
+            + "GROUP BY "
+            + "    date "
+            + "ORDER BY "
+            + "    s.start_time ASC"
+    )
+    public List<DayValue> getLast30DaysTimeSpent( String subjectName ) ;
+
+    @Query( nativeQuery=true, 
+            value = 
+            "SELECT "
+            + "    CAST( pa.start_time as DATE ) as date, "
+            + "    count( pa.id ) "
+            + "FROM "
+            + "    problem_attempt pa, "
+            + "    session s "
+            + "WHERE "
+            + "    pa.session_id = s.id AND "
+            + "    pa.start_time > DATE(DATE_SUB(NOW(), INTERVAL 30 DAY)) AND "
+            + "    s.topic_id = ?1 AND "
+            + "    s.session_type = 'Exercise' AND "
+            + "    ( pa.outcome = 'Solved' OR pa.outcome = 'Ignore' ) "
+            + "GROUP BY "
+            + "    date "
+            + "ORDER BY "
+            + "    pa.start_time ASC "
+    )
+    public List<DayValue> getLast30DaysProblemsSolved( Integer topicId ) ;
+
+    @Query( nativeQuery=true,
+            value = 
+            "SELECT "
+            + "    CAST( s.start_time as DATE ) as date, "
+            + "    sum( s.duration )/3600 as value "
+            + "FROM "
+            + "    session s "
+            + "WHERE "
+            + "    s.start_time > DATE(DATE_SUB(NOW(), INTERVAL 30 DAY)) "
+            + "GROUP BY "
+            + "    date "
+            + "ORDER BY "
+            + "    s.start_time ASC "
+    )
+    public List<DayValue> getLast30DaysTimeSpent() ;
 }
