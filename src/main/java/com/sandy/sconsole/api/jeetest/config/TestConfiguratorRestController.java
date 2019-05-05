5ab9c012ee997d7808eb089b7c8ac4328fc7702a
Sandeep Deb <deb.sandeep@gmail.com>
2019-05-05 16:48:51 +0530

    Tests are now being saved.

--- src/main/java/com/sandy/sconsole/api/jeetest/config/TestConfiguratorRestController.java
+++ src/main/java/com/sandy/sconsole/api/jeetest/config/TestConfiguratorRestController.java
@@ -1,5 +1,6 @@
 package com.sandy.sconsole.api.jeetest.config;
 
+import java.util.ArrayList ;
 import java.util.List ;
 
 import org.apache.log4j.Logger ;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody ;
 import org.springframework.web.bind.annotation.RestController ;
 
 import com.sandy.sconsole.dao.entity.TestConfigIndex ;
+import com.sandy.sconsole.dao.entity.TestQuestionBinding ;
+import com.sandy.sconsole.dao.entity.master.TestQuestion ;
 import com.sandy.sconsole.dao.repository.TestConfigIndexRepository ;
+import com.sandy.sconsole.dao.repository.TestQuestionBindingRepository ;
 
 @RestController
 public class TestConfiguratorRestController {
@@ -21,6 +25,9 @@ public class TestConfiguratorRestController {
     
     @Autowired
     private TestConfigIndexRepository tciRepo = null ;
+    
+    @Autowired
+    private TestQuestionBindingRepository tqbRepo = null ;
     
     @GetMapping( "/TestConfigurationIndex" )
     public ResponseEntity<List<TestConfigIndex>> getTestConfigIndexList() {
@@ -61,11 +68,72 @@ public class TestConfiguratorRestController {
     private Integer saveConfig( TestConfiguration config ) 
         throws Exception {
         
+        TestConfigIndex ci = null ;
+        
         // Check if this is an existing configuration.. if so, check 
-        // if this has been attempted.. if so, throw an exception.
+        // if this has been attempted.. if so, throw an exception. This
+        // situation should not arise but my OCD is kicking in..
+        if( config.getId() > 0 ) {
+            ci = tciRepo.findById( config.getId() ).get() ;
+            if( ci.getAttempted() ) {
+                log.error( "Can't modify an attempted test" ) ;
+                throw new Exception( "Can't modify an attempted test." ) ;
+            }
+            
+            // Let's delete all the question mapping rows if this is a new config
+            log.debug( "Deleting all existing test question associations" ) ;
+            tqbRepo.deleteByTestConfigId( ci.getId() ) ;
+        }
+        else {
+            ci = new TestConfigIndex() ;
+        }
         
-        // Else save and send back the new ID
+        // Save the test config index - get the id
+        ci.setNumPhyQuestions( config.getPhyQuestions().size() ) ;
+        ci.setNumChemQuestions( config.getChemQuestions().size() ) ;
+        ci.setNumMathQuestions( config.getMathQuestions().size() ) ;
         
-        return 0 ;
+        // Recreate all question mapping rows
+        List<TestQuestionBinding> tqbList = new ArrayList<>() ;
+        int duration = 0 ;
+        duration += assembleTestQuestionBinding( tqbList, config.getPhyQuestions() ) ;
+        duration += assembleTestQuestionBinding( tqbList, config.getChemQuestions() ) ;
+        duration += assembleTestQuestionBinding( tqbList, config.getMathQuestions() ) ;
+        
+        // Save the config index
+        ci.setDuration( duration ) ;
+        ci = tciRepo.save( ci ) ;
+        
+        // Associate the config index with the question bindings
+        for( TestQuestionBinding tqb : tqbList ) {
+            tqb.setTestConfig( ci ) ;
+        }
+        
+        // Save the mappings
+        tqbRepo.saveAll( tqbList ) ;
+        
+        return ci.getId() ;
+    }
+    
+    private int assembleTestQuestionBinding( List<TestQuestionBinding> tqbList,
+                                             List<TestQuestion> questionList ) {
+
+        int duration = 0 ;
+        for( int i=0; i<questionList.size(); i++ ) {
+            
+            TestQuestion q = questionList.get( i ) ;
+            
+            duration += q.getProjectedSolveTime() ;
+            
+            TestQuestionBinding tqb = new TestQuestionBinding() ;
+            tqb.setSubject( q.getSubject() ) ;
+            tqb.setTopic( q.getTopic() ) ;
+            tqb.setQuestion( q ) ;
+            tqb.setSequence( i+1 ) ;
+            
+            tqbList.add( tqb ) ;
+        }
+        
+        return duration ;
     }
 }
--- src/main/java/com/sandy/sconsole/dao/repository/TestQuestionBindingRepository.java
+++ src/main/java/com/sandy/sconsole/dao/repository/TestQuestionBindingRepository.java
@@ -1,9 +1,18 @@
 package com.sandy.sconsole.dao.repository;
 
+import javax.transaction.Transactional ;
+
+import org.springframework.data.jpa.repository.Modifying ;
+import org.springframework.data.jpa.repository.Query ;
 import org.springframework.data.repository.CrudRepository ;
 
 import com.sandy.sconsole.dao.entity.TestQuestionBinding ;
 
 public interface TestQuestionBindingRepository 
     extends CrudRepository<TestQuestionBinding, Integer> {
+    
+    @Transactional
+    @Modifying
+    @Query( "DELETE from TestQuestionBinding tqb WHERE tqb.testConfig.id = ?1" )
+    void deleteByTestConfigId( Integer testConfigId ) ;
 }
