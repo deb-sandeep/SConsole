1d7383d7988ce17fba79cbd9796833f63f65c3f3
Sandeep Deb <deb.sandeep@gmail.com>
2019-05-10 20:39:09 +0530

    UI controller linking done. Merging with route next.

--- src/main/resources/static/js/app/ng/jeetest/exam/ui.js
+++ src/main/resources/static/js/app/ng/jeetest/exam/ui.js
@@ -1,6 +1,7 @@
 sConsoleApp.controller( 'MockUIController', function( $scope, $http, $location ) {
     
 	$scope.navBarTitle = "Main" ;
+	$scope.paletteHidden = false ;
 	
 	// -----------------------------------------------------------------------
 	// --- [START] Controller initialization ---------------------------------
@@ -9,6 +10,68 @@ sConsoleApp.controller( 'MockUIController', function( $scope, $http, $location )
 	
 	// -----------------------------------------------------------------------
 	// --- [START] Scope functions -------------------------------------------
+	
+	$scope.activateSection = function( subjectName ) {
+		console.log( "Activating section = " + subjectName ) ;
+	}
+	
+	$scope.toggleQuestionPalette = function() {
+		
+	    var palette = document.getElementById( "question-palette-panel" ) ;
+	    var display = document.getElementById( "question-display-panel" ) ;
+		
+		if( $scope.paletteHidden ) {
+			palette.style.display = "block" ;
+			palette.style.width = "25%" ;
+			display.style.width = "75%" ;
+			$scope.paletteHidden = false ;
+		}
+		else {
+			palette.style.display = "none" ;
+			palette.style.width = "0%" ;
+			display.style.width = "100%" ;
+			$scope.paletteHidden = true ;
+		}
+	}
+	
+    $scope.saveAndNext = function() {
+    	console.log( "Saving answer and showing next question." ) ;
+    	// Validate if answer provided
+    }
+    
+    $scope.saveAndMarkForReview = function() {
+    	console.log( "Saving answer and marking for review. Showing next question" ) ;
+    	// Validate if answer provided
+    }
+    
+    $scope.clearResponse = function() {
+    	console.log( "Clearing response." ) ;
+    	// Stay on the same question
+    }
+    
+    $scope.markForReviewAndNext = function() {
+    	console.log( "Mark question for review and move to next" ) ;
+    }
+    
+    $scope.showNextQuestion = function() {
+    	
+    }
+    
+    $scope.showPreviousQuestion = function() {
+    	
+    }
+    
+    $scope.submitAnswers = function() {
+    	
+    }
+    
+    $scope.showQuestion = function( question ) {
+    	
+    }
+    
+    $scope.getStatusStyle = function( question ) {
+    	
+    }
 	
 	// --- [END] Scope functions
 	
