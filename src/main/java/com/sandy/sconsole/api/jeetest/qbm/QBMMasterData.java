b1c56e6877b4642eda7e7ae7f65c322998de374a
Sandeep Deb <deb.sandeep@gmail.com>
2019-06-17 18:50:45 +0530

    JEETest Question Type enumeration revisited.

--- src/main/java/com/sandy/sconsole/api/jeetest/qbm/QBMMasterData.java
+++ src/main/java/com/sandy/sconsole/api/jeetest/qbm/QBMMasterData.java
@@ -15,8 +15,8 @@ public class QBMMasterData {
     
     public static final String Q_TYPE_SCA = "SCA" ;
     public static final String Q_TYPE_MCA = "MCA" ;
-    public static final String Q_TYPE_IT  = "IT" ;
-    public static final String Q_TYPE_RNT = "RNT" ;
+    public static final String Q_TYPE_NT  = "NT" ;
+    public static final String Q_TYPE_LCT = "LCT" ;
     public static final String Q_TYPE_MMT = "MMT" ;
     
     public static final String S_TYPE_PHY   = "IIT - Physics" ;
@@ -24,7 +24,7 @@ public class QBMMasterData {
     public static final String S_TYPE_MATHS = "IIT - Maths" ;
 
     public static String[]  targetExams     = { EXAM_TYPE_MAIN, EXAM_TYPE_ADV } ;
-    public static String[]  questionTypes   = { Q_TYPE_SCA, Q_TYPE_MCA, Q_TYPE_IT, Q_TYPE_RNT, Q_TYPE_MMT } ;
+    public static String[]  questionTypes   = { Q_TYPE_SCA, Q_TYPE_MCA, Q_TYPE_NT, Q_TYPE_LCT, Q_TYPE_MMT } ;
     public static String[]  subjectNames    = { S_TYPE_PHY, S_TYPE_CHEM, S_TYPE_MATHS } ;
     public static Integer[] lateralLevel    = { 1, 2, 3, 4, 5 } ;
     public static Integer[] approxSolveTime = { 15, 30, 60, 90, 120, 180, 240, 300, 600 } ; 
