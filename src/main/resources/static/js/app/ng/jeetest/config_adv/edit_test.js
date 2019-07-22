7ca48b6637695965564aebc3d2fdf362ce5f0016
Sandeep Deb <deb.sandeep@gmail.com>
2019-07-22 12:57:43 +0530

    Adv test config iteration completed.

--- src/main/resources/static/js/app/ng/jeetest/config_adv/edit_test.js
+++ src/main/resources/static/js/app/ng/jeetest/config_adv/edit_test.js
@@ -7,17 +7,70 @@ sConsoleApp.controller( 'EditAdvTestController', function( $scope, $http, $route
 	$scope.chemTopics = [] ;
 	$scope.mathTopics = [] ;
 	
+	$scope.sectionSelectionOptions = {
+		sectionTypes : [ 'SCA', 'MCA', 'NT', 'LCT', 'MMT' ],
+		section1Type : 'MCA',
+		section2Type : 'NT',
+		section3Type : 'LCT'
+	} ;
+	
+	$scope.selectedSectionTypes = {
+		section1Type : null,
+		section2Type : null,
+		section3Type : null
+	} ;
+	
 	// -----------------------------------------------------------------------
 	// --- [START] Controller initialization ---------------------------------
 	loadQBInsights() ;
+	if( $scope.testId == -1 ) {
+		showSectionSelectionDialog() ;
+	}
+	
 	// --- [END] Controller initialization -----------------------------------
 	
 	// -----------------------------------------------------------------------
 	// --- [START] Scope functions -------------------------------------------
+	$scope.dismissSectionSelections = function() {
+		$( '#sectionSelectionDialog' ).modal( 'hide' ) ;
+		$location.path( "/" ) ;
+	}
+	
+	$scope.applySectionSelections = function() {
+		
+		var sels = [ $scope.sectionSelectionOptions.section1Type,
+					 $scope.sectionSelectionOptions.section2Type,
+		             $scope.sectionSelectionOptions.section3Type ] ;
+		
+		var types = [ sels[0] ] ;
+		for( var i=1; i<sels.length; i++ ) {
+			if( types.indexOf( sels[i] ) != -1 ) {
+				$scope.$parent.addErrorAlert( "Sections can't be of same type." ) ;
+				return ;
+			} 
+			else {
+				types.push( sels[i] ) ;
+			}
+		}
+
+		$scope.selectedSectionTypes.section1Type = sels[0] ;
+		$scope.selectedSectionTypes.section2Type = sels[1] ;
+		$scope.selectedSectionTypes.section3Type = sels[2] ;
+		
+		$( '#sectionSelectionDialog' ).modal( 'hide' ) ;
+	}
 	// --- [END] Scope functions
 	
 	// -----------------------------------------------------------------------
 	// --- [START] Local functions -------------------------------------------
+	
+	function showSectionSelectionDialog() {
+		$scope.sectionSelectionOptions.section1Type = 'MCA' ;
+		$scope.sectionSelectionOptions.section2Type = 'NT' ;
+		$scope.sectionSelectionOptions.section3Type = 'LCT' ;
+		$( '#sectionSelectionDialog' ).modal( 'show' ) ;
+	}
+	
 	function loadQBInsights() {
 		
         $scope.$parent.interactingWithServer = true ;
