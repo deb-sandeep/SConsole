438522492e127f01e6a8a18b178aa85809b82905
Sandeep Deb <deb.sandeep@gmail.com>
2019-09-21 23:57:58 +0530

    Batch synch usability enhancements

--- src/main/resources/static/js/app/ng/jeetest/qbm/search_question.js
+++ src/main/resources/static/js/app/ng/jeetest/qbm/search_question.js
@@ -8,7 +8,6 @@ sConsoleApp.controller( 'SearchQuestionController', function( $scope, $http, $lo
 	$scope.actionCmd = "" ;
 	$scope.actionCmdList = [ "", 
 		"Select All",
-		"Sync top 10",
 		"Unselect All", 
 		"Sync Selected" 
 	] ;
@@ -103,6 +102,20 @@ sConsoleApp.controller( 'SearchQuestionController', function( $scope, $http, $lo
 		fetchSearchResults( criteria ) ;
 	}
 	
+	$scope.syncTop10Questions = function() {
+        var numSelected = 0 ;
+        for( i=0; i<$scope.searchResults.length; i++ ) {
+            if( numSelected > 10 ) break ;
+            var question = $scope.searchResults[i] ;
+            if( !question.selected ) {
+                question.selected = true ;
+                numSelected++ ;
+            }
+        }
+        $scope.actionCmd = "" ;
+        syncSelectedQuestions() ;
+	}
+	
 	$scope.actionCmdChanged = function() {
 		
 		if( $scope.actionCmd == "Select All" ) {
@@ -112,19 +125,6 @@ sConsoleApp.controller( 'SearchQuestionController', function( $scope, $http, $lo
 			}
 			$scope.actionCmd = "" ;
 		}
-        else if( $scope.actionCmd == "Sync top 10" ) {
-            var numSelected = 0 ;
-            for( i=0; i<$scope.searchResults.length; i++ ) {
-                if( numSelected > 10 ) break ;
-                var question = $scope.searchResults[i] ;
-                if( !question.selected ) {
-                    question.selected = true ;
-                    numSelected++ ;
-                }
-            }
-            $scope.actionCmd = "" ;
-            syncSelectedQuestions() ;
-        }
 		else if( $scope.actionCmd == "Unselect All" ) {
 			for( i=0; i<$scope.searchResults.length; i++ ) {
 				var question = $scope.searchResults[i] ;
