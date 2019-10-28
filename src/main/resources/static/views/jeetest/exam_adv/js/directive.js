2948d85f20535e35315a94c50a94d7cd2e78ad0e
Sandeep Deb <deb.sandeep@gmail.com>
2019-10-28 21:48:22 +0530

    Section header done

--- src/main/resources/static/views/jeetest/exam_adv/js/directive.js
+++ src/main/resources/static/views/jeetest/exam_adv/js/directive.js
@@ -1,7 +1,6 @@
 sConsoleApp.directive( 'attachHoverHandlers', function() {
 
     return function( $scope, element, attributes ) {
-        console.log( "Hellow" ) ;
         if( $scope.$last ) {
            $scope.$parent.initializeSectionHovers() ;
         }
