a7a4ff2b909459f7b0d01c278e2e70f5d7181ec9
Sandeep Deb <deb.sandeep@gmail.com>
2020-03-09 16:50:03 +0530

    Adv now shows sections in instructions and unassigned Q can be viewed

--- src/main/resources/static/views/jeetest/exam_landing/js/instructions.js
+++ src/main/resources/static/views/jeetest/exam_landing/js/instructions.js
@@ -1,12 +1,16 @@
 sConsoleApp.controller( 'InstructionsController', function( $scope, $http, $location, $window ) {
     
 	$scope.$parent.navBarTitle = "Exam Instructions" ;
+	$scope.sectionDetails = [] ;
 	
 	// -----------------------------------------------------------------------
 	// --- [START] Controller initialization ---------------------------------
 	
 	if( $scope.$parent.activeTest == null ) {
 		$location.path( "/" ) ;
+	}
+	else {
+	    loadTestConfiguration( $scope.$parent.activeTest.id ) ;
 	}
 	
 	// --- [END] Controller initialization -----------------------------------
@@ -24,5 +28,31 @@ sConsoleApp.controller( 'InstructionsController', function( $scope, $http, $loca
 	// -----------------------------------------------------------------------
 	// --- [START] Local functions -------------------------------------------
 	
+    function loadTestConfiguration( testId ) {
+        
+        console.log( "Loading test configuration - " + testId ) ;
+        
+        $scope.$parent.interactingWithServer = true ;
+        $http.get( '/TestConfiguration/' + testId )
+        .then( 
+            function( response ){
+                console.log( response.data ) ;
+                var cfg = response.data ;
+                for( var i=0; i<cfg.phySectionNames.length; i++ ) {
+                    $scope.sectionDetails.push( 
+                            cfg.phySectionNames[i] + 
+                            " - " + 
+                            cfg.phySecQuestionIndices[i].length ) ;
+                }
+            }, 
+            function( error ){
+                console.log( "Error getting test configuration from server." + error ) ;
+                $scope.addErrorAlert( "Could not fetch test." ) ;
+            }
+        )
+        .finally(function() {
+            $scope.interactingWithServer = false ;
+        }) ;
+    }	
 	// --- [END] Local functions
 } ) ;
